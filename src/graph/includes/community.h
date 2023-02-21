a9593c8a2b09a503930b9e1986cf38a62dc5dc84
shaysingh818 <shalinsingh818@gmail.com>
2023-02-21 10:27:57 -0500

    start 0.2.0

--- src/graph/includes/community.h
+++ src/graph/includes/community.h
@@ -8,4 +8,8 @@ int degree_centrality(graph_t *g, node_t *root);
 int weighted_degree_centrality(graph_t *g); 
 int **kosaraju(graph_t *g, int start_vertex); 
 
+/* v.0.2.0 */
+int closeness_centrality(graph_t *g, node_t *root); 
+int page_rank(graph_t *g, node_t *root); 
+
 #endif
--- src/matrice/includes/matrix.h
+++ src/matrice/includes/matrix.h
@@ -21,5 +21,10 @@ void weighted_entry(mat_t *m, int v1, int v2, int weight);
 void directed_weighted_entry(mat_t *m, int v1, int v2, int weight);
 void print_matrix(mat_t *m);
 
+/* graph methods */
+void bfs_mat(mat_t *m, int value); 
+void dfs_mat(mat_t *m, int value); 
+void degree_centrality(mat_t *m, int value); 
+
 #endif
 
